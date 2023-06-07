6540d08cf21b0e2a28005ade6a8cc7e05ad3d9df
aldershko <iamalengeorge7@gmail.com>
2023-06-07 15:59:47 +0530

    refactor:add create note functionality

--- src/components/UI/TextInput.tsx
+++ src/components/UI/TextInput.tsx
@@ -1,77 +1,93 @@
-import React from 'react'
-import {FieldErrors,FieldValues,UseFormRegister } from 'react-hook-form'
+import React, { ReactNode, forwardRef } from "react";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
+export type TextInputProps = {
+  type: "text" | "password" | "number";
+  colorScheme: string;
+  borderScheme: string;
+  rounded: "xs" | "md" | "lg";
+  placeHolder: string;
+  variant?: "outline" | "filled" | "flushed" | "unstyled";
+  Disabled?: boolean;
+  readonly?: boolean;
+  // required?: boolean;
+  size: "lg" | "md" | "sm" | "xs";
+  name?: string;
+  register: UseFormRegister<FieldValues>;
+  error: FieldErrors<FieldValues>;
+  validationSchema?: {
+    required?: boolean;
+    requiredError?: string;
+    maxLength?: number;
+    minLength?: number;
+    pattern?: RegExp;
+    patternError?: string;
+  };
+};
 
+const borderScheme = {
+  green: " border-2 border-green-400",
+  white: " border ",
+};
 
+const colorScheme = {
+  green: " text-black bg-green-300",
+  white: " text-black bg-gray-100",
+};
 
-export type TextInputProps ={
-    type:"text" | "number" | "password";
-    placeholder: string;
-    rounded: "md" | "lg" | "xl"
-    shadow:"md" | "lg" | "xl";
-    name?:string;
-    register:UseFormRegister<FieldValues>;
-    error:FieldErrors<FieldValues>;
-    validationSchema?:{
-      required?:boolean;
-      requiredError?:string;
-      maxLength?:number;
-      minLength?:number;
-      pattern?:RegExp;
-      patterError?:string;
-    }
+const radius = {
+  xs: "rounded-xs",
+  md: "rounded-md",
+};
 
- }
+const size = {
+  lg: "h-20",
+  md: "h-16",
+  sm: "h-12",
+  xs: "h-10",
+};
 
-const radius ={
-    md:"rounded-md",
-    lg:"rounded-lg",
-    xl:"rounded-xl",
-}
-
-const dropRadius={
-    md:"shadow-md",
-    lg:"shadow-lg",
-    xl:"shadow-xl",
-}
-
-
-const TextInput = (props : TextInputProps) => {
-
-
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
   return (
     <>
-    <input className={` my-8 p-3 px-8 ${(radius as any)[props.rounded]} hover:${(dropRadius as any)[props.shadow]}`} 
-    type={props.type}
-    placeholder={props.placeholder}
-    {...props.register(props.name || '',props.validationSchema)} />
-
-    <div className='my-2 text-sm text-red-600'>
-      {props.error && props.error[props.name || ""]?.type === 'required' && (
-        <span>
-          {props.validationSchema?.requiredError?
-          props.validationSchema?.requiredError : 
-          'Field cannot be empty'}
-        </span>
-      )}
-      {props.error && props.error[props.name || ""]?.type === 'minLength' && (
-        <span>
-            Should be atleast {props.validationSchema?.minLength}
-        </span>
-      )}
-      {props.error && props.error[props.name || ""]?.type === 'pattern' && (
-        <span>
-           {props.validationSchema?.requiredError?
-          props.validationSchema?.requiredError : 
-          'Invalid pattern'}{' '}
-        </span>
-      )}
-    </div>
-
-    
-
+      <input
+        className={`${(colorScheme as any)[props.colorScheme]} ${
+          (radius as any)[props.rounded]
+        } mt-3 w-full p-2 align-middle  text-md tracking-wide text-gray-700 disabled:cursor-not-allowed disabled:bg-gray-400 ${
+          (size as any)[props.size]
+        } ${(borderScheme as any)[props.borderScheme]}`}
+        type={props.type}
+        disabled={props.Disabled}
+        readOnly={props.readonly}
+        // required={props.required}
+        placeholder={props.placeHolder}
+        {...props.register(props.name || "", props.validationSchema)}
+      />
+      {/* display errors div */}
+      <div
+        className=" my-2 text-sm text-red-600
+      "
+      >
+        {props.error && props.error[props.name || ""]?.type === "required" && (
+          <span>
+            {props.validationSchema?.requiredError
+              ? props.validationSchema?.requiredError
+              : "Field cannot be empty"}
+          </span>
+        )}
+        {props.error && props.error[props.name || ""]?.type === "minLength" && (
+          <span>Should be at least {props.validationSchema?.minLength} </span>
+        )}
+        {props.error && props.error[props.name || ""]?.type === "pattern" && (
+          <span>
+            {props.validationSchema?.patternError
+              ? props.validationSchema?.patternError
+              : "Invalid pattern"}{" "}
+          </span>
+        )}
+      </div>
     </>
-  )
-}
+  );
+});
 
-export default TextInput
+export default TextInput;
