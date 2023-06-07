36b2bc98a2495aaa28f7b4e7422bfe156e07405b
aldershko <iamalengeorge7@gmail.com>
2023-06-07 16:03:28 +0530

    refactor:add single note item

---
+++ src/components/UI/NoteItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Note } from '../Layout/MainLayout'
+
+const NoteItem = (props:Note) => {
+  return (
+    <>
+    <div className='flex p-2 m-2 flex-col rounded-md justify-center' style={{backgroundColor: props.config.color}}>
+        <div className='flex py-2 font-medium'>
+            {props.content.header}
+        </div>
+        <div className='flex font-base '>{props.content.body.bodyContent}</div>
+    </div>
+    </>
+  )
+}
+
+export default NoteItem
